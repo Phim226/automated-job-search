ffca5df457a6e3084b9e224e55fc67aa82956cda
Andrew Phimister <andyph1@hotmail.com>
2026-07-13 20:39:11 +0100

    main now inserts jobs to the database

--- src/automated_job_search/main.py
+++ src/automated_job_search/main.py
@@ -1,10 +1,17 @@
 from automated_job_search.config.config_loader import ConfigLoader
 from automated_job_search.scraper.scraper import Scraper
+from automated_job_search.definitions import JOB_DATA_DIR
+from automated_job_search.storage import ConnectionManager, JobStorageManager
 
 if __name__ == "__main__":
     config_loader = ConfigLoader()
     scraper = Scraper(config_loader)
+
+    cm = ConnectionManager(JOB_DATA_DIR/"job_database.db")
+    jsm = JobStorageManager(cm)
+    jsm.initialise_tables()
+
     spacecareers_jobs = scraper.get_jobs()
     scraper.apply_scoring(spacecareers_jobs)
 
-    print(spacecareers_jobs)
+    jsm.insert_job_summary(spacecareers_jobs)
