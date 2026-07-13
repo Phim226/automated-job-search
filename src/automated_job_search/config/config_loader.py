95e2e7b19b092b1df9e81e9b0ae8c6603e7c834a
Andrew Phimister <andyph1@hotmail.com>
2026-07-13 16:35:22 +0100

    Important directory locations defined in dedicated file

--- src/automated_job_search/config/config_loader.py
+++ src/automated_job_search/config/config_loader.py
@@ -1,24 +1,22 @@
 import json
-from pathlib import Path
 from typing import Any
 from automated_job_search.config.job import Job, Jobsite
+from automated_job_search.definitions import JOB_DATA_DIR
 
 class ConfigLoader:
 
     def __init__(self) -> None:
-        job_data_path = Path(__file__).resolve().parent.parent.parent.parent/"job_data"
-
-        with open(job_data_path/"job_sites.json", "r") as file:
+        with open(JOB_DATA_DIR/"job_sites.json", "r") as file:
             jobsites =  json.load(file)
 
         self._jobsites = dict()
         for site in jobsites:
             self._jobsites[site["scraper"]] = (Jobsite(**site))
 
-        with open(job_data_path/"disqualification.json") as file:
+        with open(JOB_DATA_DIR/"disqualification.json") as file:
             self._disqualifications: dict[str, list[str]] = json.load(file)
 
-        with open(job_data_path/"scoring.json") as file:
+        with open(JOB_DATA_DIR/"scoring.json") as file:
             self._scoring: dict[str, dict[str, int]] = json.load(file)
 
     @property
---
+++ src/automated_job_search/definitions.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+SRC_DIR = ROOT_DIR/"src"/"automated_job_search"
+JOB_DATA_DIR = ROOT_DIR/"job_data"
