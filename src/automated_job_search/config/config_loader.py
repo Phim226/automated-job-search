c68ce3a20b1e2a36b779e460c8f4649435cf3766
Andrew Phimister <andyph1@hotmail.com>
2026-07-15 23:24:40 +0100

    Functions for loading JobDetails and Job from database entries written. Functions now also take list of job information rather than single dictionaries

--- src/automated_job_search/config/config_loader.py
+++ src/automated_job_search/config/config_loader.py
@@ -1,6 +1,6 @@
 import json
 from typing import Any
-from automated_job_search.config.job import Job, Jobsite
+from automated_job_search.config.job import Job, JobDetails, Jobsite
 from automated_job_search.definitions import JOB_DATA_DIR
 
 class ConfigLoader:
@@ -31,13 +31,46 @@ class ConfigLoader:
     def scoring(self) -> dict[str, dict[str, int]]:
         return self._scoring
 
-    def load_space_careers_job(self, job_json: dict[str, Any], job_site: str) -> Job:
-        return Job(
-                job_id = job_json["id"].lower(),
-                title = job_json["title"].lower(),
-                company = job_json["employer"]["name"].lower(),
-                city = job_json["city"].lower(),
-                country = job_json["country_display"].lower(),
-                job_site = job_site,
-                score = 0,
+    def load_space_careers_job(self, job_jsons: list[dict[str, Any]]) -> list[Job]:
+        jobs = []
+        for job in job_jsons:
+            jobs.append(
+                Job(
+                    job_id = job["id"].lower(),
+                    title = job["title"].lower(),
+                    company = job["employer"]["name"].lower(),
+                    city = job["city"].lower(),
+                    country = job["country_display"].lower(),
+                    job_site = job["job_site"],
+                    score = 0,
+                )
             )
+
+        return jobs
+
+    def load_job_from_db(self, job_db_entries: list[tuple[str, str, str, str, str, str, int]]) -> list[Job]:
+        jobs = []
+        for job in job_db_entries:
+            jobs.append(Job(*job))
+
+        return jobs
+
+    def load_space_careers_job_details(self, job_info_pair: list[tuple[Job, dict[str, Any]]]) -> list[JobDetails]:
+        job_details = []
+        for job_summary, details in job_info_pair:
+            job_details.append(
+                JobDetails(
+                    *job_summary,
+                    data_posted= "",
+                    duration = "",
+                    deadline = "",
+                    rolling_deadline = True,
+                    expired = False,
+                    on_site_remote = "",
+                    description = "",
+                    application_url = "",
+                    advert_url = ""
+                )
+            )
+
+        return job_details
