c614a4cb486a6071e7b8dd0d4af5606cb8bfa388
Andrew Phimister <andyph1@hotmail.com>
2026-07-15 16:46:39 +0100

    job site table now created and populated during table initialisation

--- src/automated_job_search/storage/job_storage_manager.py
+++ src/automated_job_search/storage/job_storage_manager.py
@@ -14,6 +14,20 @@ class JobStorageManager:
         self.config_loader = config_loader
 
     def initialise_tables(self) -> list[Any]:
+        results = []
+        results.append(self.create_tables())
+        results.append(self.populate_job_sites())
+        return results
+
+    def create_tables(self) -> list[Any]:
+        job_site_creation_query = f"""
+            CREATE TABLE IF NOT EXISTS {self.JOB_SITE} (
+                name CHAR(50) PRIMARY KEY,
+                url CHAR(50),
+                api VARCHAR(255)
+            );
+        """
+
         job_summary_creation_query = f"""
             CREATE TABLE IF NOT EXISTS {self.JOB_SUMMARY} (
                 job_id CHAR(50) PRIMARY KEY,
@@ -21,13 +35,16 @@ class JobStorageManager:
                 company CHAR(50),
                 city CHAR(50),
                 country CHAR(50),
-                score INT
+                job_site CHAR(50),
+                score INT,
+                FOREIGN KEY (job_site) REFERENCES {self.JOB_SITE}(name)
             );
         """
 
         job_details_creation_query = f"""
             CREATE TABLE IF NOT EXISTS {self.JOB_DETAILS} (
-                job_id CHAR(50),
+                job_id CHAR(50) PRIMARY KEY,
+                job_site CHAR(50),
                 date_posted CHAR(50),
                 duration CHAR(50),
                 deadline CHAR(50),
@@ -36,12 +53,29 @@ class JobStorageManager:
                 advert_url VARCHAR(1000),
                 application_url VARCHAR(1000),
                 description TEXT(5000),
-                CONSTRAINT fk_job_summary
-                FOREIGN KEY (job_id)
-                REFERENCES {self.JOB_SUMMARY}(job_id)
+                FOREIGN KEY (job_id) REFERENCES {self.JOB_SUMMARY}(job_id),
+                FOREIGN KEY (job_site) REFERENCES {self.JOB_SITE}(name)
             );
         """
-        return self.cm.chain_query([job_summary_creation_query, job_details_creation_query])
+        return self.con_manager.chain_query([job_site_creation_query, job_summary_creation_query, job_details_creation_query])
+
+    def populate_job_sites(self):
+        queries: list[str] = []
+        for site in self.config_loader.jobsites.values():
+            query = f"""
+                INSERT OR IGNORE INTO {self.JOB_SITE} (
+                    name,
+                    url,
+                    api
+                ) VALUES(
+                    '{site.name}',
+                    '{site.url}',
+                    '{site.api}'
+                )
+            """
+            queries.append(query)
+
+        return self.con_manager.chain_query(queries)
 
     def reinitialise_tables(self) -> list[Any]:
         self.drop_tables()
