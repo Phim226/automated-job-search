fc7908c4b394bb5ae3451436568766d50924d107
Andrew Phimister <andyph1@hotmail.com>
2026-07-16 18:37:12 +0100

    Loading of json files removed from ConfigLoader

--- src/automated_job_search/config/config_loader.py
+++ src/automated_job_search/config/config_loader.py
@@ -1,35 +1,10 @@
-import json
 from typing import Any
 from automated_job_search.config.job import Job, JobDetails, Jobsite
-from automated_job_search.definitions import JOB_DATA_DIR
 
 class ConfigLoader:
 
-    def __init__(self) -> None:
-        with open(JOB_DATA_DIR/"job_sites.json", "r") as file:
-            jobsites =  json.load(file)
-
-        self._jobsites = dict()
-        for site in jobsites:
-            self._jobsites[site["scraper"]] = (Jobsite(**site))
-
-        with open(JOB_DATA_DIR/"disqualifiars.json") as file:
-            self._disqualifications: dict[str, list[str]] = json.load(file)
-
-        with open(JOB_DATA_DIR/"scoring.json") as file:
-            self._scoring: dict[str, dict[str, int]] = json.load(file)
-
-    @property
-    def jobsites(self) -> dict[str, Jobsite]:
-        return self._jobsites
-
-    @property
-    def disqualifications(self) -> dict[str, list[str]]:
-        return self._disqualifications
-
-    @property
-    def scoring(self) -> dict[str, dict[str, int]]:
-        return self._scoring
+    def __init__(self, job_sites: dict[str, Jobsite]) -> None:
+        self._job_sites = job_sites
 
     def load_space_careers_job(self, job_jsons: list[dict[str, Any]]) -> list[Job]:
         jobs = []
