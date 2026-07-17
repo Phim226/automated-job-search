f586f9c21f88124f8f5dbe939c22b351718bad9f
Andrew Phimister <andyph1@hotmail.com>
2026-07-17 01:13:50 +0100

    Job search moved to public function and system exits on request error

--- src/automated_job_search/main.py
+++ src/automated_job_search/main.py
@@ -1,4 +1,6 @@
 import json
+import requests
+import sys
 from automated_job_search.config.config_loader import ConfigLoader
 from automated_job_search.scraper.scraper import Scraper
 from automated_job_search.definitions import JOB_DATA_DIR
@@ -29,33 +31,46 @@ def load_filters() -> tuple[dict[str, dict[str, int]], dict[str, list[str]]]:
 
 class AutomatedJobSearch:
 
-    def __init__(self) -> None:
-        job_sites = load_sites()
-        config_loader = ConfigLoader(job_sites)
-        scraper = Scraper(job_sites)
-
-        con_manager = ConnectionManager(JOB_DATA_DIR/"job_database.db")
-        jsm = JobStorageManager(con_manager, job_sites)
-        jsm.reinitialise_tables()
-
-        filter = JobFilter(*load_filters())
-
-        jobs = config_loader.load_space_careers_job(scraper.get_jobs())
-        filtered_jobs = filter.filter_jobs(jobs)
-        filter.apply_scoring(filtered_jobs)
-
-        jsm.insert_job_summary(filtered_jobs)
-
-        top_jobs_db_records = jsm.select_top_scoring_job_summaries(10)
-        top_jobs = config_loader.load_job_from_db(top_jobs_db_records)
-
-        job_detail_pair = list(zip(top_jobs, scraper.retrieve_spacecareers_job_details(top_jobs)))
-
-        job_details = config_loader.load_space_careers_job_details(job_detail_pair)
-        jsm.insert_job_details(job_details)
-
     def search(self) -> None:
-        pass
+        self._load_json_data()
+        self._initialise_all_objects()
+
+        # TODO: Reinitialisation should be removed eventually. Tables should persis and be checked for new expirations to keep data valid
+        self.jsm.reinitialise_tables()
+
+        try:
+            jobs_dicts = self.scraper.get_jobs()
+
+        except requests.RequestException as error:
+            print(f"Job retrieval failed: {error}")
+            sys.exit(1)
+
+        jobs = self.config_loader.load_space_careers_job(jobs_dicts)
+        filtered_jobs = self.filter.filter_jobs(jobs)
+        self.filter.apply_scoring(filtered_jobs)
+
+        self.jsm.insert_job_summary(filtered_jobs)
+
+        top_jobs_db_records = self.jsm.select_top_scoring_job_summaries(10)
+        top_jobs = self.config_loader.load_job_from_db(top_jobs_db_records)
+
+        job_detail_pair = list(zip(top_jobs, self.scraper.retrieve_spacecareers_job_details(top_jobs)))
+
+        job_details = self.config_loader.load_space_careers_job_details(job_detail_pair)
+        self.jsm.insert_job_details(job_details)
+
+    def _initialise_all_objects(self):
+        self.scraper = Scraper(self.job_sites)
+        self.config_loader = ConfigLoader(self.job_sites)
+        self.con_manager = ConnectionManager(JOB_DATA_DIR/"job_database.db")
+        self.jsm = JobStorageManager(self.con_manager, self.job_sites)
+        self.filter = JobFilter(self.scoring, self.disqualifiers)
+
+    def _load_json_data(self) -> None:
+        self.job_sites = load_sites()
+        self.scoring, self.disqualifiers = load_filters()
+
 
 if __name__ == "__main__":
     ajs = AutomatedJobSearch()
+    ajs.search()
