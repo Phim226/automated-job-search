b730c7e5524abfee61343d4cc2ac0dbc9fd2b04f
Andrew Phimister <andyph1@hotmail.com>
2026-07-17 01:13:10 +0100

    timeout of 60 seconds given to requests and errors handled using raise_for_status

--- src/automated_job_search/scraper/scraper.py
+++ src/automated_job_search/scraper/scraper.py
@@ -15,20 +15,15 @@ class Scraper:
     def retrieve_spacecareers_jobs(self) -> list[dict[str, Any]]:
         api = self._jobsites["space_careers"].api
 
-        response = requests.get(api)
-        if not response.ok:
-            print(f"Request to spacecareers.uk was not successful. Code {response.status_code}")
-            return []
+        response = requests.get(api, timeout = 60)
+        response.raise_for_status()
 
         total_jobs = response.json()["count"]
         api_request = f"{api}?include_expired=false&limit={total_jobs}&offset=0"
 
-        response = requests.get(api_request)
-        if not response.ok:
-            print(f"Request to spacecareers.uk was not successful. Code {response.status_code}")
-            return []
+        response = requests.get(api_request, timeout = 60)
+        response.raise_for_status()
 
-        #advert_url = f"{self._jobsites["space_careers"].url}{job["id"]}"
         results: list[dict[str, Any]] = response.json()["results"]
         for result in results:
             result["job_site"] = self._jobsites["space_careers"].scraper
@@ -41,10 +36,8 @@ class Scraper:
         for job in jobs:
             url = f"{api}{job.job_id}/"
 
-            response = requests.get(url)
-            if not response.ok:
-                print(f"Request to spacecareers.uk was not successful. Code {response.status_code}")
-                break
+            response = requests.get(url, timeout = 60)
+            response.raise_for_status()
 
             details.append(response.json())
 
