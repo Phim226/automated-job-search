48809a549de6b1e21b4a101da60256b30d566eb7
Andrew Phimister <andyph1@hotmail.com>
2026-07-19 01:01:47 +0100

    Refactored code into private methods for higher level readability

--- src/automated_job_search/main.py
+++ src/automated_job_search/main.py
@@ -42,33 +42,8 @@ class AutomatedJobSearch:
             # TODO: Reinitialisation should be removed eventually. Tables should persist and be checked for new expirations to keep data valid
             self.jsm.reinitialise_tables()
 
-            jobs_dicts = self.scraper.get_jobs()
-
-            logging.info("Jobs scraped")
-
-            jobs = self.config_loader.load_space_careers_job(jobs_dicts)
-            filtered_jobs = self.filter.filter_jobs(jobs)
-            self.filter.apply_scoring(filtered_jobs)
-
-            self.jsm.save_job_summary(filtered_jobs)
-
-            logging.info("Job summaries saved to database")
-
-            # Retrieves (job_id, title, job_site) of jobs scoring minimum_score or higher
-            minimum_score = 10
-            top_jobs_db_records = self.jsm.select_top_scoring_job_summaries(minimum_score)
-            top_jobs_ids = [record[0] for record in top_jobs_db_records] # Job ids are required in list form for the retrieval of the job details
-
-            # The function that loads the JobDetails objects takes a list of the (job_id, title, job_site) tuples and the associated
-            # job details dictionary that is retrieved from space careers website
-            job_detail_pair = list(zip(top_jobs_db_records, self.scraper.retrieve_spacecareers_job_details(top_jobs_ids)))
-
-            logging.info("Job details scraped")
-
-            job_details = self.config_loader.load_space_careers_job_details(job_detail_pair)
-            self.jsm.save_job_details(job_details)
-
-            logging.info("Job details saved to database")
+            self._initialise_job_processing()
+            self._job_details_processing()
 
         except requests.RequestException as error:
             logging.exception(f"Job retrieval failed: {error}")
@@ -93,6 +68,35 @@ class AutomatedJobSearch:
         self.scoring = load_scoring()
         self.disqualifiers = load_disqualifiers()
 
+    def _initialise_job_processing(self) -> None:
+        jobs_dicts = self.scraper.get_jobs()
+
+        logging.info("Jobs scraped")
+
+        jobs = self.config_loader.load_space_careers_job(jobs_dicts)
+        filtered_jobs = self.filter.filter_jobs(jobs)
+        self.filter.apply_scoring(filtered_jobs)
+
+        self.jsm.save_job_summary(filtered_jobs)
+
+        logging.info("Job summaries saved to database")
+
+    def _job_details_processing(self) -> None:
+        # Retrieves (job_id, title, job_site) of jobs scoring minimum_score or higher
+        minimum_score = 10
+        top_jobs_db_records = self.jsm.select_top_scoring_job_summaries(minimum_score)
+        top_jobs_ids = [record[0] for record in top_jobs_db_records] # Job ids are required in list form for the retrieval of the job details
+
+        # The function that loads the JobDetails objects takes a list of the (job_id, title, job_site) tuples and the associated
+        # job details dictionary that is retrieved from space careers website
+        job_detail_pair = list(zip(top_jobs_db_records, self.scraper.retrieve_spacecareers_job_details(top_jobs_ids)))
+
+        logging.info("Job details scraped")
+
+        job_details = self.config_loader.load_space_careers_job_details(job_detail_pair)
+        self.jsm.save_job_details(job_details)
+
+        logging.info("Job details saved to database")
 
 if __name__ == "__main__":
     logging.basicConfig(filename = "job-scraper.log", encoding = "utf-8", level = logging.DEBUG)
